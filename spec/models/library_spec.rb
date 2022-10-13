558320db0b8d4de74bfa19bb0fe023e5ec13fe77
Rich Kaht <adamselene1776@protonmail.com>
2022-10-13 07:43:37 -0600

    Add Feature/Test: Library has many books

--- spec/models/library_spec.rb
+++ spec/models/library_spec.rb
@@ -1,5 +1,11 @@
 require "rails_helper"
-require 'date'
 
 RSpec.describe Library, type: :model do
+  before :each do
+    @alexendria = Library.create!(name: 'Alexendria', public_library: false, employees: 1)
+    @potter = @alexendria.books.create!(name: "Harry Potter and The Sorcer's Stone", checked_out: true, pages: 224)
+    @potter = @alexendria.books.create!(name: "Harry Potter and The Chamber of Secrets", checked_out: false, pages: 357)
+  end
+  it { should have_many(:books) }
+  
 end
