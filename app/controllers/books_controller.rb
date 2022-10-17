3cb584a74b1f57d4eeed45f9cb3d326060ad32ee
Rich Kaht <adamselene1776@protonmail.com>
2022-10-16 18:36:18 -0600

    Modify books index to only show books with checked_out as true

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,6 +1,7 @@
 class BooksController < ApplicationController
   def index
     @books = Book.all
+    @books = @books.select {|book| book.checked_out}
   end
   
   def show
--- spec/features/books/index_spec.rb
+++ spec/features/books/index_spec.rb
@@ -7,22 +7,18 @@ RSpec.describe Book do
     @potter2 = @alexandria.books.create!(name: "Harry Potter and The Chamber of Secrets", checked_out: false, pages: 357)
   end
   
-  # As a visitor
-  # When I visit '/child_table_name'
-  # Then I see each Child in the system including the Child's attributes
-  # (data from each column that is on the child table)
-  
   describe 'As a visitor' do
     describe "When I visit '/books'" do
-      it "I see the name of each Book record in the system including attributes" do
+      it "I see the name of each Book record in the system including attributes
+      only if the boolean value is true" do
         visit "/books"
         
         expect(page).to have_content("Harry Potter and The Sorcer's Stone")
         expect(page).to have_content('Checked Out? true')
         expect(page).to have_content('Number of Pages: 224')
-        expect(page).to have_content("Harry Potter and The Chamber of Secrets")
-        expect(page).to have_content('Checked Out? false')
-        expect(page).to have_content('Number of Pages: 357')
+        expect(page).to_not have_content("Harry Potter and The Chamber of Secrets")
+        expect(page).to_not have_content('Checked Out? false')
+        expect(page).to_not have_content('Number of Pages: 357')
       end
       
       it "I see a link at the top of the page that takes me to the Child Index" do
