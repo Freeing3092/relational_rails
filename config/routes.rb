dceabe9e73b11c9e3b592e0d2f21f3a49670d216
Rich Kaht <adamselene1776@protonmail.com>
2022-10-17 18:11:24 -0600

    Re-organize routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,19 +4,18 @@ Rails.application.routes.draw do
   get '/libraries', to: 'libraries#index'
   get '/libraries/new', to: 'libraries#new'
   get '/libraries/:id', to: 'libraries#show'
+  post '/libraries/new', to: 'libraries#create'
+  
   get '/libraries/:id/books', to: 'libraries#library_books'
   get '/libraries/:id/books/new', to: 'books#new'
+  post '/libraries/:id/books/new', to: 'books#create'
+  get 'libraries/:id/edit', to: 'libraries#edit'
+  patch '/libraries/:id/edit', to: 'libraries#update'
   
   get '/books', to: 'books#index'
   get '/books/:id', to: 'books#show'
+  
   get '/books/:id/edit', to: 'books#edit'
   patch '/books/:id/edit', to: 'books#update'
   
-  post '/libraries/:id/books/new', to: 'books#create'
-  
-  post '/libraries/new', to: 'libraries#create'
-  
-  get 'libraries/:id/edit', to: 'libraries#edit'
-  
-  patch '/libraries/:id/edit', to: 'libraries#update'
 end
